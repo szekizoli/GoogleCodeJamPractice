c550c1492f0b5469746cd6a9c875a56f0300414f
Zoltan <szekizoli@gmail.com>
2020-04-11 18:01:04 +0100

    [2020] Round 1A: Pascal walk (After reading analysis)

--- 2020_Round_1A/b.cpp
+++ 2020_Round_1A/b.cpp
@@ -52,99 +52,101 @@ ostream& operator<<(ostream& os, vector<T> const& x) {
   return os;
 }
 
+template<typename T>
+ostream& operator<<(ostream& os, vector<vector<T>> const& x) {
+  for(size_t i = 0; i < x.size(); ++i) os << x[i] << endl;
+  return os;
+}
+
 // Algorithm
 
-vector<long> next(long n_r, vector<long> const& v) {
+vector<long> next(vector<long> const& v) {
   if (v.size() == 1) return vector<long> {1, 1};
-  vector<long> r;
-  r.push_back(1);
+  vector<long> r {1};
   for (size_t i = 0; i < v.size() - 1; ++i) {
     r.push_back(v[i] + v[i+1]);
   }
-  if (n_r % 2 == 0) {
-    r.push_back(v.back() + v.back());
-  }
+  r.push_back(1);
   return r;
 }
 
-size_t idx(vector<long> const& v, size_t b) {
-  return (v.size() - b >= 1) ? v.size() - b - 1 : 0;
-}
-
-long last_2(vector<long> const& v) {
-  return v.back() + v[v.size()-2];
-}
-
-void solve_large(input const& in) {
-  long sum = 0;
-  long n_r = 1;
-  long n_s = 1;
-  int dir = 0;
-  cout << endl;
+vector<vector<long>> generate() {
   vector<vector<long>> p;
   p.emplace_back(vector<long>{1});
-  while (in.N > sum + n_s) {
-    vector<long>& r = p.back();
-    cout << r << endl;
-    cout << n_r << " " << r.size() << " (" << r.back() << ")" << endl;
-    sum += r.back();
-    n_s *= 2;
-    ++n_r;
-    p.emplace_back(next(n_r, p.back()));
+  for (long r = 0; r < 60; ++r) {
+    p.emplace_back(next(p[r]));
   }
-  vector<long>& r = p.back();
-  cout << r << endl;
-  cout << sum << endl;
-  --n_r;
-  for (; n_r >= 0 && in.N >= sum + p[n_r][p[n_r].size()-2]; --n_r) {
-    cout << n_r << " " << p[n_r].size()-1 << endl;
-    sum += p[n_r][p[n_r].size()-2]; 
-  }
-  /*for (size_t i = r.size() - 1 ; i > 0 && in.N > sum + r[i-1]; --i) {
-    cout << n_r << " " << i-1 << endl;
-    sum += r[i-1];
-    cout << sum << endl;
-  }*/
-  cout << sum << endl;
-  /*while (sum < in.N) {
-    if (dir == 0) {
-      cout << n_r << " " << "1" << endl;
-    } else {
-      cout << n_r << " " << n_r << endl;
-    }
-    ++sum;
-    ++n_r;
-  }*/
+  return p;
 }
 
-void solve(input const& in) {
-  cout << endl;
-  cout << "1 1" << endl;
-  if (in.N == 1) { return; }
-  if (in.N > 1000) { solve_large(in); return; }
-  long sum = 1;
-  long c_r = 2;
-  while (sum + 2*c_r + 1 <= in.N) {
-    sum += 2*c_r + 1;
-    cout << c_r << " 1" << endl;
-    cout << c_r << " 2" << endl;
-    ++c_r;
-    cout << c_r << " 2" << endl;
-    cout << c_r << " 1" << endl;
-    ++c_r; 
+struct context {
+  vector<vector<long>> const& tri;
+  int r = 1;
+  int k = 1;
+  long s = 0;
+
+  void select() { s -= value(); }
+  context& operator++() {
+    ++r;
+    if (!at_front()) ++k;
+    return *this;
   }
-  while (sum < in.N) {
-    cout << c_r << " 1" << endl;
-    ++c_r; ++sum;
+
+  bool operator()() const { return s > 0; }
+
+  bool at_front() const { return k == 1; }
+  bool at_back()  const { return k == r; }
+
+  long value() const { return tri[r-1][k-1]; }
+  vector<long> const& operator[](size_t i) const { return tri[i]; }
+};
+
+ostream& operator<<(ostream& os, context const& x) {
+  return os << x.r << " " << x.k;
+}
+
+const long LIMIT = 30;
+
+void solve(input const& in, vector<vector<long>> const& tri) {
+  long N = in.N - LIMIT;
+  context ctx {tri, 1, 1, in.N};
+  cout << ctx << endl; 
+  ctx.select();
+  for (long i = 1; i < LIMIT && ctx() && in.N > LIMIT; ++i) {
+    ++ctx;
+    if (((1 << i) & N) > 0) {
+      if (ctx.at_front()) {
+        for (size_t j = 0; j < ctx[i].size(); ++j) {
+          cout << ctx << endl;
+          ctx.select();
+          if (!ctx.at_back()) ++ctx.k;
+        }
+      } else {
+        for (size_t j = 0; j < ctx[i].size(); ++j) {
+          cout << ctx << endl;
+          ctx.select();
+          if (!ctx.at_front()) --ctx.k;
+        }
+      } 
+    } else {
+      cout << ctx << endl;
+      ctx.select();
+    }
+  }
+
+  while (ctx()) {
+    cout << ++ctx << endl;
+    ctx.select();
   }
 }
 
 int main() {
+  vector<vector<long>> tri = generate();
   int T = 0; cin >> T;
   for (int t = 0; t < T; ++t) {
     input in; cin >> in;
-    cout << "Case #" << t+1 << ": ";
-    solve(in);
+    cout << "Case #" << t+1 << ": " << endl;
+    solve(in, tri);
   }
   return 0;
 }
