19b909d13efa4d55bfe6af569eb7a152d3cb4bb8
Zoltan Szekeres <zszekeres@apple.com>
2021-04-10 14:13:02 +0100

    2021 Qualification round (live)

---
+++ 2021_Qualification_Round/a.cpp
@@ -0,0 +1,82 @@
+#include<algorithm>
+#include<cmath>
+#include<cstdint>
+#include<cctype>
+#include<iostream>
+#include<iomanip>
+#include<iterator>
+#include<limits>
+#include<map>
+#include<memory>
+#include<numeric>
+#include<set>
+#include<sstream>
+#include<string>
+#include<utility>
+#include<vector>
+
+using namespace std;
+
+// I/O
+
+struct input {
+  int n;
+  vector<int> l;
+};
+
+template<typename T0, typename T1>
+istream& operator>>(istream& is, pair<T0, T1>& x) {
+  is >> x.first >> x.second;
+  return is;
+}
+
+template<typename T>
+istream& operator>>(istream& is, vector<T>& x) {
+  for(size_t i = 0; i < x.size(); ++i) is >> x[i];
+  return is;
+}
+
+istream& operator>>(istream& is, input& in) {
+  is >> in.n;
+  in.l.resize(in.n);
+  return is >> in.l;
+}
+
+template<typename T0, typename T1>
+ostream& operator<<(ostream& os, pair<T0, T1>& x) {
+  os << x.first << x.second;
+  return os;
+}
+
+template<typename T>
+ostream& operator<<(ostream& os, vector<T> const& x) {
+  for(size_t i = 0; i < x.size(); ++i) os << x[i] << " ";
+  return os;
+}
+
+// Algorithm
+
+void solve(input & in) {
+  int count = 0;
+  for (int i = 0; i < in.l.size() - 1; ++i) {
+      auto first = begin(in.l) + i;
+      auto min_e = std::min_element(first, end(in.l));
+      if (min_e == end(in.l)) {
+        break;
+      }
+      count += std::distance(first, min_e + 1);
+      std::reverse(first, min_e + 1);
+  }
+  cout << count;
+}
+
+int main() {
+  int T = 0; cin >> T;
+  for (int t = 0; t < T; ++t) {
+    input in; cin >> in;
+    cout << "Case #" << t+1 << ": ";
+    solve(in);
+    cout << endl;
+  }
+  return 0;
+}
---
+++ 2021_Qualification_Round/c_ok.cpp
@@ -0,0 +1,116 @@
+#include<algorithm>
+#include<cmath>
+#include<cstdint>
+#include<cctype>
+#include<iostream>
+#include<iomanip>
+#include<iterator>
+#include<limits>
+#include<map>
+#include<memory>
+#include<numeric>
+#include<set>
+#include<sstream>
+#include<string>
+#include<utility>
+#include<vector>
+
+using namespace std;
+
+// I/O
+
+struct input {
+    int t, n, q;
+};
+
+template<typename T0, typename T1>
+istream& operator>>(istream& is, pair<T0, T1>& x) {
+  is >> x.first >> x.second;
+  return is;
+}
+
+template<typename T>
+istream& operator>>(istream& is, vector<T>& x) {
+  for(size_t i = 0; i < x.size(); ++i) is >> x[i];
+  return is;
+}
+
+istream& operator>>(istream& is, input& in) {
+  return is >> in.t >> in.n >> in.q;
+}
+
+template<typename T0, typename T1>
+ostream& operator<<(ostream& os, pair<T0, T1>& x) {
+  os << x.first << x.second;
+  return os;
+}
+
+template<typename T>
+ostream& operator<<(ostream& os, vector<T> const& x) {
+  for(size_t i = 0; i < x.size(); ++i) os << x[i] << " ";
+  return os;
+}
+
+using I = vector<int>::iterator;
+
+int T = 0, Q = 0;
+
+int ask(int a, int b, int c) {
+	++Q;
+	cerr << "ask (" << T << ", "<< Q << "): " <<  a << " " << b << " " << c << endl;
+  cout << a << " " << b << " " << c << endl;
+  cin >> a;
+	cerr << "answer: " << a << endl;
+	return a;
+}
+
+pair<I, bool> place(int n, I f, I l) {
+	auto dist = std::distance(f, l);
+	cerr << "dist: " << dist << endl;
+  I mid = f + (dist-1) / 2;
+	int a = ask(n, *mid, *(mid+1));
+	if (a == -1) {
+		return make_pair(f, false);
+	}
+	if (a == n) {
+		return make_pair(mid + 1, true);
+	}
+  if (a == *mid) {
+		if (dist <= 2) return make_pair(mid, true);
+		return place(n, f, mid);
+	}
+
+	if (dist <= 2)  return make_pair(mid + 2, true);
+	return place(n, mid, l);
+}
+
+// Algorithm
+bool solve(input const& in) {
+  vector<int> v = {1, 2, 3};
+  int a = ask(1, 2, 3);
+  if (a == -1) { return true; }
+  if (a != 2) { std::swap(v[1], v[a-1]); }
+  for (int i = 3; i < in.n; ++i) {
+		 cerr << "v: " <<  v << endl << endl;
+     pair<I, bool> p = place(i + 1, begin(v), end(v));
+     if (!p.second) { return true; }
+     v.insert(p.first, i+1);
+  }
+	cerr << "guess: " << v << endl;
+  cout << v << endl;
+	cin >> a;
+	cerr << "answer: " << a << endl;
+  return a != 1;
+}
+
+int main() {
+  input in; cin >> in;
+	cerr << in.t << " " << in.n << " " << in.q << endl;
+  for (int t = 0; t < in.t; ++t) {
+		++T;
+  	cerr << "test case: " << t+1 << endl;
+    bool stop = solve(in);
+    if (stop) { break; }
+  }
+  return 0;
+}
